0e61defa572156464a54fcf3f7978ccd0e3683b7
Nicolae Teslaru <teslaruzero@gmail.com>
2019-02-22 12:49:53 +0100

    Add Step One

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+    <div>
+        <h1>Reading List</h1>
+        <p>Much component. Very wow</p>
+        <p>Here will be the things to read</p>
+    </div>
+)
