93bfc71ccedeac0c73b05a676f9ba3792dbbece9
Nicolae Teslaru <teslaruzero@gmail.com>
2019-02-23 12:13:10 +0100

    Add Step Six

--- src/components/article.js
+++ src/components/article.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styles from "./article.module.css";
-import { Link } from "gatsby";
 
 export default props => (
   <div>
-    <Link to={props.url}>
+    <a href={props.url}>
       <h3 className={styles.title}>{props.title}</h3>
-    </Link>
+    </a>
     <small>{props.date}</small>
     <p className={styles.description}>{props.description}</p>
   </div>
